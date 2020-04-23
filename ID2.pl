37587b37975c62a659b4e70006d20a64b6ac58b3
Archmage『 Enderian 』 <62359703+ArchmageEnderian@users.noreply.github.com>
2020-04-23 21:26:53 +0300

    Вторая попытка
    Хорошо, это так не работает, я понял

--- ID2.pl
+++ ID2.pl
@@ -1,8 +1,9 @@
 ili_mn(A,B,Fin):-prov(A,B,Fin, []).
 
 prov([],_,Fin, Fin):-!.
-prov([H|T],B,Fin, Fin1):-prov2(H, B, Fin, Fin1), prov(T, B, Fin, Fin1).
+prov([H|T],B,Fin, Fin1):-prov2(H, B, Fin, Fin1, []), prov(T, B, Fin, Fin1).
 
-prov2(Q,[], _, Fin1):-append(Fin1,[Q],Fin1), !.
-prov2(Q,[H|T],Fin,Fin1):- not(H = Q), prov2(Q, T, Fin, Fin1).
-prov2(_,_,_,_):-!.
+prov2(Q,[], _, Fin1,Fin2):-append(Fin1,[Q],Fin2),sli(Fin1, Fin2), !.
+prov2(Q,[H|T],Fin,Fin1,Fin2):- not(H = Q), prov2(Q, T, Fin, Fin1, Fin2).
+prov2(_,_,_,_,_):-!.
+sli(Fin, Fin):-!.
