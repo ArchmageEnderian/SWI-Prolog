4d9b6278f39c6c97193fc1a23caa29b3fbdde0c0
Archmage『 Enderian 』 <62359703+ArchmageEnderian@users.noreply.github.com>
2020-04-24 21:49:34 +0300

    Все на полнейшей динамике

--- ID7.pl
+++ ID7.pl
@@ -17,56 +17,62 @@ read_high(_):-read_str(Lang), name(X,Lang),read(Y),asserta(high(X,Y)), get0(Sym)
 decl_r(X,Y):-repeat,(dec1(X,Y) -> (nl,write(X),nl,write(Y),write("."), retract(dec1(X,Y))) ;X=nil,Y=nil).
 
 pr_d_t:-tell('d:/GitHub/SWI-Prolog/Fukinator_lite_data/decl.txt'),decl_r(X,_),X=nil,told.
-pr_d_s:-see('d:/GitHub/SWI-Prolog/Fukinator_lite_data/decl.txt'), get0(Sym), read_high1(Sym),seen.
-read_high1(-1):-!.
-read_high1(_):-	read_str(Lang),name(X,Lang),read(Y),asserta(dec1(X,Y)),
-				get0(Sym),read_high1(Sym).
+pr_d_s:-see('d:/GitHub/SWI-Prolog/Fukinator_lite_data/decl.txt'), get0(Sym), read_dec1(Sym),seen.
+read_dec1(-1):-!.
+read_dec1(_):- read_str(Lang),name(X,Lang),read(Y),asserta(dec1(X,Y)),get0(Sym),read_dec1(Sym).
 
 
-interpret(ruby,1).
-interpret(python,1).
-interpret(f_sharp,1).
-interpret(prolog,1).
-interpret(c_sharp,0).
-interpret(c_plu_plus,0).
-interpret(c,0).
-interpret(asm,0).
+:-dynamic interpret/2.
+interpret_r(X,Y):-repeat,(interpret(X,Y) -> (nl,write(X),nl,write(Y),write("."), retract(interpret(X,Y))); X=nil, Y=nil).
 
-oop(ruby,3).
-oop(c_sharp,3).
-oop(python,2).
-oop(c_plu_plus,2).
-oop(f_sharp,1).
-oop(prolog,1).
-oop(c,0).
-oop(asm,0).
+pr_in_t:-tell('d:/GitHub/SWI-Prolog/Fukinator_lite_data/interpret.txt'), interpret_r(X,_), X=nil, told.       /*запись*/
+pr_in_s:-see('d:/GitHub/SWI-Prolog/Fukinator_lite_data/interpret.txt'), get0(Sym), read_interpret(Sym), seen. /*чтение*/
 
-cross(ruby,1).
-cross(python,1).
-cross(c_plu_plus,1).
-cross(prolog,1).
-cross(c,1).
-cross(asm,1).
-cross(c_sharp,0).
-cross(f_sharp,0).
+read_interpret(-1):-!.
+read_interpret(_):-read_str(Lang), name(X,Lang),read(Y),asserta(interpret(X,Y)), get0(Sym),read_interpret(Sym).
 
-visual(c_sharp,3).
-visual(ruby,2).
-visual(python,2).
-visual(c_plu_plus,2).
-visual(f_sharp,2).
-visual(prolog,1).
-visual(c,0).
-visual(asm,0).
 
-mobile(c_sharp,0).
-mobile(ruby,0).
-mobile(python,0).
-mobile(c_plu_plus,0).
-mobile(f_sharp,0).
-mobile(prolog,0).
-mobile(c,0).
-mobile(asm,0).
+:-dynamic oop/2.
+oop_r(X,Y):-repeat,(oop(X,Y) -> (nl,write(X),nl,write(Y),write("."), retract(oop(X,Y))); X=nil, Y=nil).
+
+pr_oop_t:-tell('d:/GitHub/SWI-Prolog/Fukinator_lite_data/oop.txt'), oop_r(X,_), X=nil, told.       /*запись*/
+pr_oop_s:-see('d:/GitHub/SWI-Prolog/Fukinator_lite_data/oop.txt'), get0(Sym), read_oop(Sym), seen. /*чтение*/
+
+read_oop(-1):-!.
+read_oop(_):-read_str(Lang), name(X,Lang),read(Y),asserta(oop(X,Y)), get0(Sym),read_oop(Sym).
+
+
+:-dynamic cross/2.
+cross_r(X,Y):-repeat,(cross(X,Y) -> (nl,write(X),nl,write(Y),write("."), retract(cross(X,Y))); X=nil, Y=nil).
+
+pr_cr_t:-tell('d:/GitHub/SWI-Prolog/Fukinator_lite_data/cross.txt'), cross_r(X,_), X=nil, told.       /*запись*/
+pr_cr_s:-see('d:/GitHub/SWI-Prolog/Fukinator_lite_data/cross.txt'), get0(Sym), read_cross(Sym), seen. /*чтение*/
+
+read_cross(-1):-!.
+read_cross(_):-read_str(Lang), name(X,Lang),read(Y),asserta(cross(X,Y)), get0(Sym),read_cross(Sym).
+
+
+:-dynamic visual/2.
+visual_r(X,Y):-repeat,(visual(X,Y) -> (nl,write(X),nl,write(Y),write("."), retract(visual(X,Y))); X=nil, Y=nil).
+
+pr_vi_t:-tell('d:/GitHub/SWI-Prolog/Fukinator_lite_data/visual.txt'), visual_r(X,_), X=nil, told.       /*запись*/
+pr_vi_s:-see('d:/GitHub/SWI-Prolog/Fukinator_lite_data/visual.txt'), get0(Sym), read_visual(Sym), seen. /*чтение*/
+
+read_visual(-1):-!.
+read_visual(_):-read_str(Lang), name(X,Lang),read(Y),asserta(visual(X,Y)), get0(Sym),read_visual(Sym).
+
+
+:-dynamic mobile/2.
+mobile_r(X,Y):-repeat,(mobile(X,Y) -> (nl,write(X),nl,write(Y),write("."), retract(mobile(X,Y))); X=nil, Y=nil).
+
+pr_mo_t:-tell('d:/GitHub/SWI-Prolog/Fukinator_lite_data/mobile.txt'), mobile_r(X,_), X=nil, told.       /*запись*/
+pr_mo_s:-see('d:/GitHub/SWI-Prolog/Fukinator_lite_data/mobile.txt'), get0(Sym), read_mobile(Sym), seen. /*чтение*/
+
+read_mobile(-1):-!.
+read_mobile(_):-read_str(Lang), name(X,Lang),read(Y),asserta(mobile(X,Y)), get0(Sym),read_mobile(Sym).
+
+
+
 
 question1(X1):-	write("Is your language high level?"),nl,
 				write("1. Yes"),nl,
@@ -109,14 +115,19 @@ question7(X7):-	write("Is your language for mobile phones?"),nl,
 
 
 
-aki:-pr_h_s,pr_d_s, question1(X1),question2(X2),question3(X3),question4(X4),question5(X5),question6(X6),question7(X7),
+aki:-pr_h_s, pr_d_s, pr_in_s, pr_oop_s, pr_oop_s, pr_vi_s, pr_mo_s,
+    question1(X1),question2(X2),question3(X3),question4(X4),question5(X5),question6(X6),question7(X7),
     prov(X1,X2,X3,X4,X5,X6,X7).
 
 prov(X1,X2,X3,X4,X5,X6,X7):-otvet(X1,X2,X3,X4,X5,X6,X7),!.
 prov(X1,X2,X3,X4,X5,X6,X7):-add(X1,X2,X3,X4,X5,X6,X7),!.
 
 otvet(X1,X2,X3,X4,X5,X6,X7):-high(X,X1),dec1(X,X2),interpret(X,X3),oop(X,X4),cross(X,X5),visual(X,X6),mobile(X,X7),write(X),!.
-add(X1,X2,X3,X4,X5,X6,X7):- write("’орошо, ты мен€ победил. я сдаюсь!\n¬ведите загаданного персонажа"),read(Q), asserta(high(Q,X1)).
+add(X1,X2,X3,X4,X5,X6,X7):- write("’орошо, ты мен€ победил. я сдаюсь!\n¬ведите загаданного персонажа"),read(Q), asserta(high(Q,X1)), asserta(dec1(Q,X2)), asserta(interpret(Q,X3)), asserta(oop(Q,X4)), asserta(cross(Q,X5)), asserta(visual(Q,X6)),
+asserta(mobile(Q,X7)), reload.
+
+reload:-pr_h_t, pr_d_t, pr_in_t, pr_oop_t, pr_oop_t, pr_vi_t, pr_mo_t,
+pr_h_s, pr_d_s, pr_in_s, pr_oop_s, pr_oop_s, pr_vi_s, pr_mo_s.
 
 
 pr_test:-read(X), (X is 1) -> (write("Yes")); write("No").
