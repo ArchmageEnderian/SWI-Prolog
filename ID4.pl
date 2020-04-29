060bcef87dbba98b4565cc24b82ec72df1f7442e
Archmage『 Enderian 』 <62359703+ArchmageEnderian@users.noreply.github.com>
2020-04-29 22:05:42 +0300

    Добавил 3 новых предиката для решения

--- ID4.pl
+++ ID4.pl
@@ -1,17 +1,27 @@
 in_list([El|_],El).
 in_list([_|T],El):-in_list(T,El).
 
+sleva_next(A,B,[C]):-fail.
+sleva_next(A,B,[B|[A|T]]).
+sleva_next(A,B,[_|List]):-sleva_next(A,B,List).
 
-log_zad:- List1[_,_,_],
+sprava_next(A,B,[C]):-fail.
+sprava_next(A,B,[A|[B|T]]).
+sprava_next(A,B,[_|List]):-sprava_next(A,B,List).
+
+naprotiv(A,B1,List):-sprava_next(A,B,List),sprava_next(B,B1,List).
+
+log_zad:- List1=[_,_,_],
     (
         (in_list(List1,[kuznetsov,tokar,_]);in_list(List1,[kuznetsov,slesar,_]);in_list(List1,[kuznetsov,rezchik,_])),
         (in_list(List1,[tokarev,kuznets,_]);in_list(List1,[tokarev,slesar,_]);in_list(List1,[tokarev,rezchik,_])),
         (in_list(List1,[slesarev,kuznets,_]);in_list(List1,[slesarev,tokar,_]);in_list(List1,[slesarev,rezchik,_])),
         (in_list(List1,[rezchikov,kuznets,_]);in_list(List1,[rezchikov,tokar,_]);in_list(List1,[rezchikov,slesar,_])),
-        in_list(List1,[kuznetsov,slesar,naprotiv]),in_list(List1,[rezchikov,rezchik,naprotiv]),
-        in_list(List1,[slesarev,tokar,sprava])
-    ),
-    write(List1).
+
+         naprotiv(kuznetsov,_,List1), naprotiv(rezchikov,_,List1),
+         sprava_next(slesarev,_,List1), sleva_next(_,Who1,List1)
+     ),
+    write(Who1).
 
 
 
