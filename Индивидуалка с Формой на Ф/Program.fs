964f9dd88c9a1020e98e8725fcf38b840ae28ee2
『 Enderian 』 <iggor.malakhov@gmail.com>
2020-05-13 19:29:34 +0300

    Моя остановка

--- Индивидуалка с Формой на Ф/Program.fs
+++ Индивидуалка с Формой на Ф/Program.fs
@@ -4,13 +4,12 @@ open System.Windows.Forms
 let MainForm = new Form(Width = 400, Height = 300, Text = "F# Main Form")
 
 
-let ProgressBar1 = new ProgressBar(Dock=DockStyle.Top)
 let ScrollBar1 = new TrackBar(Top = 50, Maximum = 100, Width = 400)
-let Button1 = new Button(Top = 100, Left = 150, Text="Перейти на форму 3")
-MainForm.Controls.Add(ProgressBar1)
+let E1M1Button = new Button(Top = 100, Left = 150, Text="Ад на земле")
+
 MainForm.Controls.Add(ScrollBar1)
-MainForm.Controls.Add(Button1)
-let Change _ = ProgressBar1.Value <- ScrollBar1.Value
+MainForm.Controls.Add(E1M1Button)
+let Change _ = E1M1Button.Size.Width <- ScrollBar1.Value
 let _ = ScrollBar1.ValueChanged.Add(Change)
 
 
