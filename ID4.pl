d157b41d84a5a99f6dbc0ff169ebc71555c0ecce
Archmage『 Enderian 』 <62359703+ArchmageEnderian@users.noreply.github.com>
2020-05-19 20:46:57 +0300

    Ага, оно работает!
    НЕУЖЕЛИ, вроде вышло

--- ID4.pl
+++ ID4.pl
@@ -1,5 +1,5 @@
 permute([],[]).
-permute([H|Tail],P):-permute(Tail,T),insert(H,T,P).
+permute([H|T],P):-permute(T,T1),insert(H,T1,P).
 insert(Y,XZ,XYZ):-append(X,Z,XZ),append(X,[Y],XY),append(XY,Z,XYZ).
 
 naprotiv(A,B,[_,A,_,B]).
@@ -9,7 +9,23 @@ naprotiv(B,A,[_,A,_,B]).
 sprava(A,B,[A,B,_,_]).
 sprava(A,B,[_,_,A,B]).
 
-sleva(A,B,L):-sprava(B,A,L).
+sleva(A,B,List):-sprava(B,A,List).
+
+find(Ans):-
+        permute([kuznecov,tokarev,slesarev,rezchikov],[A,B,C,D]),
+        permute([kuznec,tokar,slesar,rezchik],[Q1,Q2,Q3,Q4]),
+
+        List=[m(A,Q1),m(B,Q2),m(C,Q3),m(D,Q4)],
+        member(m(S1,kuznec),List),not(S1=kuznecov),
+        member(m(S2,tokar),List),not(S2=tokarev),
+        member(m(S3,slesar),List),not(S3=slesarev),
+        member(m(S4,rezchik),List),
+
+        Familii=[A,B,C,D],
+        naprotiv(kuznecov,S3,Familii), /*1. Напротив Кузнецова сидит слесарь.*/
+        sprava(slesarev,S2,Familii), /*3. Справа от Слесарева сидит токарь.*/
+        naprotiv(rezchikov,S4,Familii), /*2. Напротив Резчикова сидит резчик.*/
+        sleva(Ans,S1,Familii). /*Кто сидит слева от кузнеца?*/
 
 /*Четыре человека играют в домино. Их фамилии Кузнецов, Токарев, Слесарев и Резчиков.
 Профессия каждого игрока соответствует фамилии одного из других игроков.
