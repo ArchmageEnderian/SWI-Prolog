74cbdf71aecba47c9c5b4e9ae2e8ece442b4b1e3
『 Enderian 』 <iggor.malakhov@gmail.com>
2020-06-08 14:59:02 +0300

    Инда 12*
    Консольное? Ну что такое?!

---
+++ inda 13/inda 13/Program.fs
@@ -0,0 +1,11 @@
+﻿let converter (str:string) = 
+    let rec reconverter (strList:string list) intList = 
+        match strList with
+        |[] -> intList
+        |h::t -> reconverter t ((System.Convert.ToInt32 h)::intList)
+    reconverter (List.ofArray (str.Split(' '))) []
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (converter "1 8 3 16 5 6")
+    0 // return an integer exit code
