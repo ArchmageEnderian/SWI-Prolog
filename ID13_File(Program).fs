966907736c2ef300825237882d0a18f1c5e6c32e
Archmage『 Enderian 』 <62359703+ArchmageEnderian@users.noreply.github.com>
2020-06-08 19:19:23 +0300

    Составил обработку кортежей

---
+++ ID13_File(Program).fs
@@ -0,0 +1,49 @@
+﻿let form = new System.Windows.Forms.Form(Width = 300, Height = 180)
+
+let label = new System.Windows.Forms.Label(Text = "Введите список кортежей:", Width = 280, Height = 20)
+label.Location <- new System.Drawing.Point(10, 10)
+form.Controls.Add(label)
+
+let KortegTextBox = new System.Windows.Forms.TextBox(Text = "7 3 4 5 6; 2 3 4 6 7; 2 3 4 5 6; 4 3 10 4 5", Width = 265, Height = 25)
+KortegTextBox.Location <- new System.Drawing.Point(10, 30)
+form.Controls.Add(KortegTextBox)
+
+let obrabotka_odnogo (kor:string) =
+    match kor.Trim().Split(' ') with
+
+    | [|a; b; c; d; e|] ->
+        let convert = List.ofArray [|a; b; c; d; e|]
+        let ToIntConvert = [ System.Convert.ToInt32(a); System.Convert.ToInt32(b); System.Convert.ToInt32(c); System.Convert.ToInt32(d); System.Convert.ToInt32(e) ]
+        if (List.forall (fun x -> x < 10) ToIntConvert) then
+            (ToIntConvert.Item(0), ToIntConvert.Item(1), ToIntConvert.Item(2), ToIntConvert.Item(3), ToIntConvert.Item(4))
+        else
+            (-1, -1, -1, -1, -1)
+
+    | _ -> (-1, -1, -1, -1, -1) //Если не условие, то нафиг его
+
+let obrabotka_vsa (str:string) =
+    let rec Razdelenie list razdelInt =
+        match list with
+        | [] -> List.filter ( fun x -> not (x = (-1, -1, -1, -1, -1))   ) razdelInt
+        | h::t ->
+            let TekKorteg = obrabotka_odnogo(h)
+            Razdelenie t (List.append razdelInt [TekKorteg])
+
+    Razdelenie (List.ofArray (str.Split(';'))) []
+
+//*****************************************************************
+let rec VstavkaDlyaSortirovki i = function
+| h::t -> min h i::(VstavkaDlyaSortirovki (max h i) t)
+| _ -> [i]
+
+let SortVstavkami l = List.foldBack VstavkaDlyaSortirovki l []
+//*****************************************************************
+
+let button = new System.Windows.Forms.Button(Text = "Посчитать", Width = 265)
+button.Location <- new System.Drawing.Point(10, 50)
+form.Controls.Add(button)
+
+[<EntryPoint>]
+let main argv =
+    System.Windows.Forms.Application.Run(form)
+    0
